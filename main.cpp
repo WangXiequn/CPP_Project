62a8e7f57998476eab9ba4d1fee9568bb587fd19
czh <czh1300643800@gmail.com>
2021-06-04 17:07:11 +0800

    001

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Matrix.h"
+using std::cout;
+int main() 
+{
+    Matrix<int> matrix(2,2);
+    
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
