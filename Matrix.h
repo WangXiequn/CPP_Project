7abea34539dd67708e323e91b159310d1042870c
WangXiequn <wangxiequn@126.com>
2021-06-04 16:54:45 +0800

    fix bug

--- Matrix.h
+++ Matrix.h
@@ -6,23 +6,51 @@
 #define CPP_PROJECT_MATRIX_H
 
 #include <bits/stdc++.h>
+#include <ostream>
+
+
 using namespace std;
-template<typename T>class Matrix{
+template<typename T>
+class Matrix{
 private:
     int row,col;
     vector<vector<T>> mat;
 public:
     explicit Matrix<T>(const vector<vector< T>> & mat);
     explicit Matrix<T>(int row,int col);
+    bool change_item_by_index(int row_index,int col_index,T value);
+    friend ostream &operator<<(ostream &os, const Matrix<T> &matrix){
+        for (int i = 0; i < matrix.row; ++i) {
+            os<<"[";
+            for (int j = 0; j < matrix.col; ++j) {
+                os<<matrix.mat[i][j];
+                if (j==matrix.col-1){
+                    os<<"]";
+                } else{
+                    os<<",";
+                }
+            }
+            os<<endl;
+        }
+        return os;
+    }
+
 };
-
-
 
 
 template<typename T>
 Matrix<T>::Matrix(int row, int col) {
     this->row = row;
     this->col = col;
+    vector<vector<T>> s;
+    this->mat = s;
+    for (int i = 0; i < row; ++i) {
+        vector<T> temp;
+        this->mat.push_back(temp);
+        for (int j = 0; j < col; ++j) {
+            this->mat[i].push_back(static_cast<T>(0));
+        }
+    }
 }
 
 template<typename T>
@@ -35,9 +63,9 @@ Matrix<T>::Matrix(const vector <vector<T>> &mat) {
         }
     }
     vector<vector<T>> s;
-    vector<T> temp;
     this->mat = s;
     for (int i = 0; i < mat.size(); ++i) {
+        vector<T> temp;
         this->mat.push_back(temp);
         for (int j = 0; j < s[i].size(); ++j) {
             this->mat[i].push_back(mat[i][j]);
@@ -47,6 +75,17 @@ Matrix<T>::Matrix(const vector <vector<T>> &mat) {
         }
     }
 }
+
+template<typename T>
+bool Matrix<T>::change_item_by_index(int row_index, int col_index, T value) {
+    if (row_index>this->row||col_index>this->col||row_index<1||col_index<1){
+        return false;
+    }
+    this->mat[row_index-1][col_index-1] = value;
+    return true;
+}
+
+
 
 
 #endif //CPP_PROJECT_MATRIX_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include "Matrix.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Matrix<int> mat(3,2);
+    mat.change_item_by_index(1,2,4);
+    mat.change_item_by_index(2,2,6);
+    std::cout<<mat;
     return 0;
 }
