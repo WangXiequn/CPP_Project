7433511fc9712ecb7cf125ed178e52ae897def01
WangXiequn <wangxiequn@126.com>
2021-06-04 15:27:00 +0800

    first commit

--- Matrix.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 11383 on 2021/5/26.
-//
-
-#include "Matrix.h"
--- Matrix.h
+++ Matrix.h
@@ -1,14 +1,47 @@
 //
-// Created by 11383 on 2021/5/26.
+// Created by 11383 on 2021/5/13.
 //
 
-#ifndef CPP_PROJECT_MATRIX_H
-#define CPP_PROJECT_MATRIX_H
+#ifndef CS205_PROJECT_MATRIX_H
+#define CS205_PROJECT_MATRIX_H
 
-
-class Matrix {
-
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T>class Matrix{
+private:
+    int row,col;
+    vector<vector<T>> mat;
+public:
+    explicit Matrix<T>(const vector<vector< T>> & mat);
+    explicit Matrix<T>(int row,int col);
 };
 
+template<typename T>
 
-#endif //CPP_PROJECT_MATRIX_H
+
+template<typename T>
+Matrix<T>::Matrix<T>(int row, int col) {
+
+}
+
+template<typename T>
+Matrix<T>::Matrix<T>(const vector <vector<T>> &mat) {
+    this->row = mat.size();
+    int max = 0;
+    for (vector<T> s:mat) {
+        if (max<s.size()){
+            max = s.size();
+        }
+    }
+    vector<vector<T>> s;
+    this->mat = s;
+    for (int i = 0; i < mat.size(); ++i) {
+        this->mat.push_back(vector<T>);
+        for (int j = 0; j < s[i].size(); ++j) {
+            this->mat[i].push_back(mat[i][j]);
+        }
+    }
+}
+
+
+#endif //CS205_PROJECT_MATRIX_H
